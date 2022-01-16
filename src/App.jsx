efc4db20149178abefce99cc32842f87ae991991
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2022-01-15 19:31:32 -0500

    final update before moving to Waves music full application. messed with useEffect a bit. did not implement. commented out parts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CreateTweet from './components/CreateTweet';
 import TweetList from './components/TweetList';
 
