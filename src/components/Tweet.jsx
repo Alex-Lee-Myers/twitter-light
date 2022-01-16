d0b79ed8b1b12e6584237595623fb9a94238f7fd
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2022-01-15 19:22:37 -0500

    played with SASS and styled-components a bit. moving onto Waves (music app) next

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,4 +1,6 @@
 import {useState} from 'react';
+// import '../styles/Tweet.scss';
+import styled from 'styled-components';
 
 const Tweet = ({name, tweet, tweets, setTweets}) => {
     // useState Variables
@@ -11,13 +13,29 @@ const Tweet = ({name, tweet, tweets, setTweets}) => {
 
 
     return (
-        <div className="tweet">
+        <TweetStyled>
             <h2>{name}</h2>
             <p>{tweet.message}</p>
             <button onClick={deleteTweet}>Delete</button>
             <button>Like</button>
-        </div>
+        </TweetStyled>
     );
 };
+
+//styled-components
+const TweetStyled = styled.div`
+    background-color: blue;
+    color: white;
+    padding: 10px;
+    margin: 10px;
+    border-radius: 5px;
+    box-shadow: 0px 0px 5px black;
+    h2 {
+        font-size: 1.5rem;
+    }
+    p {
+        font-size: 1rem;
+    }
+`;
 
 export default Tweet;
